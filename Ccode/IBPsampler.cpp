7419ba154d20822800276f7354d14dc9f7abd138
Melanie Pradier <melanie.fpradier@gmail.com>
2016-12-27 12:35:54 +0100

    Debugging

--- Ccode/IBPsampler.cpp
+++ Ccode/IBPsampler.cpp
@@ -136,6 +136,7 @@ void mexFunction( int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[] ) {
         }
     }
 
+    int idx_tmp;
     for (int d=0; d<D; d++){
         if (C[d] =='o') {
             idx_tmp = 1;
@@ -145,13 +146,13 @@ void mexFunction( int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[] ) {
         gsl_matrix_view Bd_view =  gsl_matrix_submatrix (B[d], 0, 0, Kest, idx_tmp);
         gsl_matrix *BT=gsl_matrix_alloc(idx_tmp,Kest);
         gsl_matrix_transpose_memcpy (BT, &Bd_view.matrix);;
-        for (int i=0;i<Kest*maxR;i++){
-            if (C[d]!='c' & i<Kest){
-                pB[D*i+d]=(BT)->data[i];
-            }else if (C[d]=='c' & i<Kest*idx_tmp){
-                pB[D*i+d]=(BT)->data[i];
-            }
-        }
+#        for (int i=0;i<Kest*maxR;i++){
+#            if (C[d]!='c' & i<Kest){
+#                pB[D*i+d]=(BT)->data[i];
+#            }else if (C[d]=='c' & i<Kest*idx_tmp){
+#                pB[D*i+d]=(BT)->data[i];
+#            }
+#        }
         gsl_matrix_free(BT);
     }
 
