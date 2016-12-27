d78eef2d2b322cde228c0e427087bd46e89cdcf4
Melanie Pradier <melanie.fpradier@gmail.com>
2016-12-27 12:07:51 +0100

    Still debugging

--- Ccode/IBPsampler.cpp
+++ Ccode/IBPsampler.cpp
@@ -152,7 +152,7 @@ void mexFunction( int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[] ) {
 
     for (int d=0; d<D; d++){
         for (int i=0;i<maxR;i++){
-            if (C[d]=='o' & i<R[d]){
+            if (C[d]=='o' & i<(R[d]-1)){
                 pT[D*i+d]=(theta[d])->data[i];
             }
         }
