f69fa2a3bb602c76cbcb9862b646c73b8887e930
melaniefp <melanie.fpradier@gmail.com>
2017-03-02 14:49:51 +0100

    Added script to verify call to GLFM library

--- GLFMpython/check_GLFM_infer_call.py
+++ GLFMpython/check_GLFM_infer_call.py
@@ -1,15 +1,20 @@
 import numpy as np
 import sys
-
 sys.path.append('../Ccode/wrapper_python/')
-
+import pdb
 import GLFM
 
-import pdb
+# --------------------------------------------------
+# Function to verify good behavior of GLFM library
+# --------------------------------------------------
 
-Z = np.array([[1.0,0],[1,1],[1,1]])
-X = np.array([[1.0, 1, -0.3, 1],[6, 2, 3.8, 23],[11, 3, 4.1, 4]])
-C = 'PoGN'
+print('\n\n# -------------------------------------------------')
+print('# SCRIPT TO CHECK CALL TO GLFM LIBRARY')
+print('# -------------------------------------------------\n')
+
+Z = np.array([[1.0,0],[1,1],[1,1]]) # dimensions (N,K)
+X = np.array([[1.0, 1, -0.3, 1],[6, 2, 3.8, 23],[11, 3, 4.1, 4]]) # (N,D)
+C = 'goGN'
 W = 2.0 / np.max(X,0)
 
 print 'First, in Python'
@@ -20,7 +25,6 @@ X2 = np.ascontiguousarray(X.transpose())
 Z2 = np.ascontiguousarray(Z.transpose())
 
 print '\nNow, inside C\n'
-#pdb.set_trace()
 (Z_out,B_out,Theta_out) = GLFM.infer(X2,C,Z2,W)
 
 print '\nBack to Python\n'
@@ -30,5 +34,7 @@ print B_out
 print "\n"
 print Theta_out
 
-print "SUCCESSFUL"
+print('\n\n# -------------------')
+print "# SUCCESSFUL"
+print('# -------------------')
 
