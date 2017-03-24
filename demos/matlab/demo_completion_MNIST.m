5f04883923be4c9bf103c5c3244848790c73bec7
melaniefp <melanie.fpradier@gmail.com>
2017-03-24 19:56:50 +0100

    Playing with demo scripts

--- demos/matlab/demo_completion_MNIST.m
+++ demos/matlab/demo_completion_MNIST.m
@@ -26,7 +26,7 @@ data.C = C;
 
 %% Define parameter default values for algorithm
 params.bias = 1;
-params.s2Y = 1.0;
+params.s2Y = 0.2;
 params.s2B = 1.0;
 params.alpha = 1.0;
 params.Niter = 10;
@@ -40,8 +40,8 @@ hidden = IBPsampler_run(data,[],params); % no need to initialize Z
 
 X_map = IBPsampler_MAP(data.C, hidden.Z, hidden);
 % visualization random image
-idx = randi(N,1);
-figure(1);
+idx = 10; %randi(N,1);
+figure;
 subplot(1,3,1); imagesc(reshape(Xtrue(idx,:),sqrt(784),sqrt(784)) );
 subplot(1,3,2); imagesc(reshape(data.X(idx,:),sqrt(784),sqrt(784)) );
 subplot(1,3,3); imagesc(reshape(X_map(idx,:),sqrt(784),sqrt(784)) );
--- demos/matlab/demo_toy_images.m
+++ demos/matlab/demo_toy_images.m
@@ -18,11 +18,11 @@ hidden.Z = Zini;
 
 % define params
 params.missing = -10;
-params.s2Y = 1;    % Variance of the Gaussian prior on the auxiliary variables (pseudoo-observations) Y
+params.s2Y = 2;    % Variance of the Gaussian prior on the auxiliary variables (pseudoo-observations) Y
 params.s2u = .005;
 params.s2B = 1;      % Variance of the Gaussian prior of the weigting matrices B
 params.alpha = 1;    % Concentration parameter of the IBP
-params.Niter = 5000;  % Number of iterations for the gibbs sampler
+params.Niter = 100;  % Number of iterations for the gibbs sampler
 params.maxK = 10;
 params.bias = 0;
 
