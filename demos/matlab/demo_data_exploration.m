f11b2c77662f9a55f86b26787a82a39eb9779043
melaniefp <melanie.fpradier@gmail.com>
2017-03-29 13:06:58 +0200

    Playing

--- demos/matlab/demo_data_exploration.m
+++ demos/matlab/demo_data_exploration.m
@@ -52,21 +52,12 @@ hidden.Z = Zini; % N*D
 
 %% DEFINE PARAMS
 params.missing = -1;
-<<<<<<< Updated upstream
-params.s2Y = 0.5;     % Variance of the Gaussian prior on the auxiliary variables (pseudoo-observations) Y
-params.s2u = .001;  % Auxiliary variance
-params.s2B = 0.1;   % Variance of the Gaussian prior of the weigting matrices B
-params.alpha = 10;   % Concentration parameter of the IBP
-if ~isfield(params,'Niter')
-    params.Niter = 100; % Number of iterations for the gibbs sampler
-end
-=======
-params.s2Y = 1;   % Variance of the Gaussian prior on the auxiliary variables (pseudoo-observations) Y
+params.s2Y = 0.5;   % Variance of the Gaussian prior on the auxiliary variables (pseudoo-observations) Y
 params.s2u = .005;   % Auxiliary variance
-params.s2B = 0.5;   % Variance of the Gaussian prior of the weigting matrices B
+params.s2B = 0.1;   % Variance of the Gaussian prior of the weigting matrices B
 params.alpha = 1;   % Concentration parameter of the IBP
-params.Niter = 100;   % Number of iterations for the gibbs sampler
->>>>>>> Stashed changes
+params.Niter = 1000;   % Number of iterations for the gibbs sampler
+
 params.maxK = 10;
 params.bias = 1;
 params.func = 2*ones(1,D);
--- demos/matlab/demo_data_exploration_reduced.m
+++ demos/matlab/demo_data_exploration_reduced.m
@@ -60,7 +60,7 @@ hidden.Z = Zini; % N*D
 params.missing = -1;
 params.s2Y = 0.5;       % Variance of the Gaussian prior on the auxiliary variables (pseudoo-observations) Y
 params.s2u = .001;      % Auxiliary variance
-params.s2B = 0.1;       % Variance of the Gaussian prior of the weigting matrices B
+params.s2B = 0.5;       % Variance of the Gaussian prior of the weigting matrices B
 params.alpha = 10;      % Concentration parameter of the IBP
 if ~isfield(params,'Niter')
     params.Niter = 1000; % Number of iterations for the gibbs sampler
