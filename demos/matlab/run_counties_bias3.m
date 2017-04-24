9525faac18e5c1e520d0f91ba507d4e71f2bac62
melaniefp <melanie.fpradier@gmail.com>
2017-04-24 15:35:00 +0200

    Removed unnecessary files

--- demos/matlab/run_counties_bias3.m
+++
@@ -1,6 +0,0 @@
-for ll=1:5
-    params.simId = ll;
-    params.Niter = 10000;
-    params.save = 1;
-    demo_counties_bias3;
-end
--- demos/matlab/run_prostate.m
+++
@@ -1,7 +0,0 @@
-for ll=1:5
-    params.simId = ll;
-    params.Niter = 1000;
-    params.save = 1;
-    params.s2B = 1;
-    demo_data_exploration;
-end
