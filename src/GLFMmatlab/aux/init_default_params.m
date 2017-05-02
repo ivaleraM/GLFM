7adf58e44136dadb8bfc35c5764ffc241d6a7738
Melanie Pradier <melanie.fpradier@gmail.com>
2017-05-02 16:56:31 +0200

    Cleaned unnecessary parameters inside init_default_params

--- src/GLFMmatlab/aux/init_default_params.m
+++ src/GLFMmatlab/aux/init_default_params.m
@@ -7,7 +7,6 @@ function params = init_default_params(data, params)
     if ~isfield(params,'missing'), params.missing = -1; end % value for missings
     if ~isfield(params,'alpha'), params.alpha = 1; end % Concentration parameter for the IBP
     if ~isfield(params,'bias'), params.bias = 0; end % number of latent features that should not be sampled
-    if ~isfield(params,'s2Y'), params.s2Y = 0.5; end % Variance of the Gaussian prior on the auxiliary variables (pseudoo-observations) Y
     if ~isfield(params,'s2u'), params.s2u = 0.01; end % variance of auxiliary noise
     if ~isfield(params,'s2B'), params.s2B = 1; end % Variance of the Gaussian prior of the weigting matrices B
     if ~isfield(params,'Niter'), params.Niter = 1000; end % number of iterations to run
