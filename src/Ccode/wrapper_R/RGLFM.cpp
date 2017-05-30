bc1e262381464ffe1b32d54499b218848e07a4ff
Maria Lomeli <maria@103.134.248.10.in-addr.arpa.private.cam.ac.uk>
2017-05-30 17:54:35 +0100

    edits to incomplete code

--- src/Ccode/wrapper_R/RGLFM.cpp
+++ src/Ccode/wrapper_R/RGLFM.cpp
@@ -1,3 +1,4 @@
+// [[Rcpp::depends(RcppGSL)]]
 #include <Rcpp.h>
 #include <RcppGSL.h> 
 #include <gsl/gsl_sf_exp.h>
@@ -9,8 +10,19 @@
 #include <gsl/gsl_math.h>
 #include "gsl/gsl_cdf.h"
 #include "gsl/gsl_randist.h"
+
 using namespace Rcpp;
 // [[Rcpp::export]]
 #include "../core/GeneralFunctions.cpp"
-#include "../core/InferenceFunctions.cpp"
+#include "../core/InferenceFunctions.cpp
+
+List GLFM(NumericMatrix X, CharacterVector C, NumericMatrix Z, NumericMatrix B, double theta, int R, NumericVector weights, int maxR, int bias, int N, int D, int K, double alpha, double s2Y, double S2B, double d2u, int maxK, int Nsim  )
+{
+return List::create(B_out, Z_out,theta_out);
+}
+
+
+// initialisation
+//RcppGSL::matrix_view Xview = gsl_matrix_view_array(X_dou, D,N);
+
 
