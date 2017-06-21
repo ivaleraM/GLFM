1ae4871e25d2592c112958ba972061e16fac39bc
Maria Lomeli <maria@131.154.248.10.in-addr.arpa.private.cam.ac.uk>
2017-06-21 14:13:28 +0100

    added R function init_default_params, incomplete

---
+++ src/GLFMR/init_default_params.R
@@ -0,0 +1,33 @@
+#' Function to initialize or complete params structure with all
+#' simulation parameters and hyperparameters for the GLFM
+#' data is an input parameter in order to be able to initialize
+#' params with data-based values
+
+init_default_params<-function(data,params)
+{
+  params_names<-c("missing","alpha","bias","s2u","s2B","Niter","maxK","verbose","numS","t","t_1","dt_1")
+}
+idx_to_fill<-which(params_names %in% params)
+
+filled_param_names<-paste("params",params_names[idx_to_fill],sep=".")
+param_values<-list(-1,1,0,0.01,1,1000,dim(data)[2],1,100,list(1,dim(data)[2]),list(1,dim(data)[2]),list(1,dim(data)[2]))
+names(param_values)<-param_names
+
+# Regresa una lista de parametros con los valores iniciales que se requieran.
+function params = init_default_params(data, params)
+
+
+if ~isfield(params,'missing'), params.missing = -1; end % value for missings
+if ~isfield(params,'alpha'), params.alpha = 1; end % Concentration parameter for the IBP
+if ~isfield(params,'bias'), params.bias = 0; end % number of latent features that should not be sampled
+if ~isfield(params,'s2u'), params.s2u = 0.01; end % variance of auxiliary noise
+if ~isfield(params,'s2B'), params.s2B = 1; end % Variance of the Gaussian prior of the weigting matrices B
+if ~isfield(params,'Niter'), params.Niter = 1000; end % number of iterations to run
+if ~isfield(params,'maxK'), params.maxK = size(data.X,2); end % max number of latent features for memory allocation inside C++ routine
+if ~isfield(params,'verbose'), params.verbose = 1; end % plot info in command line
+if ~isfield(params,'numS'), params.numS = 100; end % number of points to plot in PDF
+
+% parameters for optional external transformation
+if ~isfield(params,'t'), params.t = cell(1,size(data.X,2) ); end % eventual external transform of obs. X = params.t{d}(Xraw)
+if ~isfield(params,'t_1'), params.t_1 = cell(1,size(data.X,2) ); end % inverse transform
+if ~isfield(params,'dt_1'), params.dt_1 = cell(1,size(data.X,2) ); end % derivative of the inverse transform
