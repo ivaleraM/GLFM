8c5c635422ec3c16e18291eeb5e9497b4de831a3
Maria Lomeli <maria@131.154.248.10.in-addr.arpa.private.cam.ac.uk>
2017-06-21 15:58:53 +0100

    GLFM_infer.R edits

--- src/GLFMR/GLFM_infer.R
+++ src/GLFMR/GLFM_infer.R
@@ -1,6 +1,7 @@
 #var1<-c("HOLA")
 # sapply(var1, tolower)
 #varargin is a list
+#' @return A list with posterior draws
 
 GLFM_infer<-function(data,varargin){
   varargin_size<-length(varargin)
@@ -11,14 +12,19 @@ GLFM_infer<-function(data,varargin){
   else if(varargin_size<3)
   {
     hidden<-varargin[1]
-    switch(varargin_size,params <- init_default_params(data, c()) ,params <- init_default_params(data, varargin[2]))
+    switch(varargin_size,params <- init_default_params(data, c()) ,params <- init_default_params(data, unlist(varargin[2])))
   }
   else
   {
     stop("Incorrect number of input parameters: should be 0, 1 or 2")
   }
+  D <- dim(data$X)[2]
+  N <- dim(data$X)[1]
+  # From the posterior
+  return(list("Z"=hidden))
+}
  
   
   
   
-}
+  
--- src/GLFMR/init_default_params.R
+++ src/GLFMR/init_default_params.R
@@ -6,15 +6,17 @@
 #' @param  t_1: inverse transform
 #' @param dt_1:derivative of the inverse transform
 
-init_default_params<-function(data,params)
-{
+init_default_params<-function(data,params){
+
+if (length(params)>0){
 params_names<-c("missing","alpha","bias","s2u","s2B","Niter","maxK","verbose","numS","t","t_1","dt_1")
 idx_to_fill<-which(params_names %in% params)
-
 filled_param_names<-paste("params",params_names[idx_to_fill],sep=".")
 param_values<-list(-1,1,0,0.01,1,1000,dim(data)[2],1,100,list(1,dim(data$X)[2]),list(1,dim(data$X)[2]),list(1,dim(data$X)[2]))
 #names(param_values)<-param_names
 params_to_return<-param_values[idx_to_fill]
 names(params_to_return)<-filled_param_names
 return(params_to_return)
+}
+
 }
