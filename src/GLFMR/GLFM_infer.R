682bd0b016f1dda362a08c11f06c7396c19de88b
Maria Lomeli <maria@131.154.248.10.in-addr.arpa.private.cam.ac.uk>
2017-06-21 13:55:44 +0100

    added R functions folder and GLFM_infer.R

--- src/GLFMR/GLFM_infer.R
+++ src/GLFMR/GLFM_infer.R
@@ -1,26 +1,24 @@
 #var1<-c("HOLA")
 # sapply(var1, tolower)
+#varargin is a list
 
 GLFM_infer<-function(data,varargin){
-  x <- "red"
-  > switch(x, red="cloth", size=5, name="table")
   varargin_size<-length(varargin)
-  switch(varargin_size,"0"=hidden <-c(),"1"="bu","2"="bu1")
+  if(varargin_size==0){
+    hidden<-c()
+    params <- init_default_params(data, c())
+  }
+  else if(varargin_size<3)
+  {
+    hidden<-varargin[1]
+    switch(varargin_size,params <- init_default_params(data, c()) ,params <- init_default_params(data, varargin[2]))
+  }
+  else
+  {
+    stop("Incorrect number of input parameters: should be 0, 1 or 2")
+  }
+ 
   
-  switch length(varargin)
-  case 0
-  hidden = [];
-  % initialize default values for params
-  params = init_default_params(data, []);
   
-  case 1, hidden = varargin{1};
-  params = init_default_params(data, []);
-  
-  case 2, hidden = varargin{1}; params = varargin{2};
-  params = init_default_params(data, params); % eventually complete params structure
-  
-  otherwise
-  error('Incorrect number of input parameters: should be 1, 2 or 3');
-  end
   
 }
