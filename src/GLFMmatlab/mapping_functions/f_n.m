da132f88bd4ec62a834b02fa9eac0cbf070e962c
ivaleraM <isabel.valera.martinez@gmail.com>
2019-08-21 09:56:22 +0200

    Correcting mapping functions

--- src/GLFMmatlab/mapping_functions/f_n.m
+++ src/GLFMmatlab/mapping_functions/f_n.m
@@ -4,7 +4,7 @@ function x = f_n(y, mu, w)
     if (w == 0)
         error('scaling factor should never be 0');
     end
-    x = floor( log( exp(y) + 1 )./w + mu );
+    x = floor( f_p(y, mu, w) );
     %if sum(x == 0)>0 % TODO: verify if that would be a problem % DONE, OK
     %    warning('when transforming y to x, x has been rounded to zero (count data)');
     %end
